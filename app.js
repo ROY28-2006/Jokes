e4ecc61b260ea2579f8f313b971e1b1f6b0439b9
ROY28-2006 <praas9248@gmail.com>
2026-02-14 00:30:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const apiUrl = "https://v2.jokeapi.dev/joke/Programming,Dark,Spooky?type=single"
+
+const jokeBody = document.getElementById("joke");
+const btn = document.getElementById("getjokes");
+
+
+
+let getjokes = ()=>{
+    
+    fetch(apiUrl)
+    .then(data => data.json())
+    .then(item => (jokeBody.textContent= `${item.joke}`));
+    
+    
+}
+
+btn.addEventListener("click", getjokes);
