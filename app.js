aade15f35c80700b72269739eae7e7131ed7c2fd
ROY28-2006 <praas9248@gmail.com>
2026-02-14 13:25:49 +0530

    Refined modals actions

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const apiUrl = "https://v2.jokeapi.dev/joke/Programming,Dark,Spooky?type=single"
-
+const dialog = document.querySelector(".wrapper");
 const jokeBody = document.getElementById("joke");
 const btn = document.getElementById("getjokes");
 
-alert("arpan gendu")
+const closeBtn = document.getElementById("close");
 
-let getjokes = ()=>{
-    
+let getjokes = () => {
     fetch(apiUrl)
-    .then(data => data.json())
-    .then(item => (jokeBody.textContent= `${item.joke}`));
-    
-    
+        .then(data => data.json())
+        .then(item => {
+            jokeBody.textContent = `${item.joke}`;
+            dialog.showModal();
+        })
+        .catch(error => console.error("Error fetching joke:", error));
 }
 
 btn.addEventListener("click", getjokes);
+closeBtn.addEventListener("click", () => dialog.close());
