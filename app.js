efc8344607d4bc6bc192848fe0bbf2aac39bc4fc
ROY28-2006 <praas9248@gmail.com>
2026-02-14 13:21:48 +0530

    Added modal

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const apiUrl = "https://v2.jokeapi.dev/joke/Programming,Dark,Spooky?type=single"
 const jokeBody = document.getElementById("joke");
 const btn = document.getElementById("getjokes");
 
-
+alert("arpan gendu")
 
 let getjokes = ()=>{
     
